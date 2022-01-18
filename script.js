e40da8f8926e030ff12713f4e70322a500e60a7e
Mike-Brown5 <mikeaxe5@outlook.com>
2022-01-18 06:42:46 +0200

    adding some more actions

---
+++ script.js
@@ -0,0 +1,6 @@
+var select = document.getElementById("Esclations_reqs");
+var value = select.options[select.selectedIndex].value;
+console.log(value)
+    function myFunction(){
+        alert("You selected some text!");
+    }
